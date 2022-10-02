cbe07be4c3b03561a421904637cdf022c6e3f462
zakirhossen23 <43494914+SteveThijssen20@users.noreply.github.com>
2022-10-02 15:06:04 +0600

    code

---
+++ config.js
@@ -0,0 +1,12 @@
+
+function getConfig () {
+
+  return {
+    networkId: 'testnet',
+    nodeUrl: 'https://rpc.testnet.near.org',
+    walletUrl: 'https://wallet.testnet.near.org',
+    helperUrl: 'https://helper.testnet.near.org'
+  }
+}
+
+module.exports = getConfig
